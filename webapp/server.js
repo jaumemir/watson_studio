d6d95fdf7f7d1f5bddd052963c1dd5df02c469a7
JAUME MIRALLES SOLE <jaume.miralles@es.ibm.com>
2020-01-08 22:11:17 +0100

    adjust server.js

--- webapp/server.js
+++ webapp/server.js
@@ -84,6 +84,6 @@ app.post('/predict', function(request, response1){
 });
 
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
 
 console.log('Running at Port 3000');
